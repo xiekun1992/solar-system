25cb954f7a2db40bcaccbe8c62f442305d213984
xiekun <xiekun@f3dt.com>
2018-03-05 10:12:58 +0800

    添加天体相关数据，并添加轨道，倾角，光源等设置

---
+++ data.js
@@ -0,0 +1,99 @@
+// https://wc.yooooo.us/wiki/太阳系
+
+const MINUTE = 60;
+const HOUR = 60 * MINUTE;
+const DAY = HOUR * 23 + 56 * MINUTE; // 一个地球日的秒数
+const YEAR = 365.24 * DAY;
+const AU = 1.5 * Math.pow(10, 8); // 地球到太阳的距离150,000,000km
+
+let data = {
+  sun: {
+    equatorialRadius: 696000, // 赤道半径/km
+    trackRadius: null || 0, // 轨道半径/AU
+    orbitalInclination: null, // 轨道倾角/度
+    equatorialInclination: 7.25, // 赤道倾角
+    revolutionPeriod: null || 0, // 公转周期
+    rotationCycle: 25.38 * DAY // 自转周期
+  },
+  mercury: {
+    equatorialRadius: 2440,
+    trackRadius: 0.3871 * AU,
+    orbitalInclination: 7.005,
+    equatorialInclination: 0,
+    revolutionPeriod: 87.97 * DAY,
+    rotationCycle: 59 * DAY
+  },
+  venus: {
+    equatorialRadius: 6052,
+    trackRadius: 0.7233 * AU,
+    orbitalInclination: 3.395,
+    equatorialInclination: 177.4,
+    revolutionPeriod: 225 * DAY,
+    rotationCycle: 243 * DAY
+  },
+  earth: {
+    equatorialRadius: 6378,
+    trackRadius: 1 * AU,
+    orbitalInclination: 0,
+    equatorialInclination: 23.44,
+    revolutionPeriod: YEAR,
+    rotationCycle: DAY
+  },
+  mars: {
+    equatorialRadius: 3397,
+    trackRadius: 1.5237 * AU,
+    orbitalInclination: 1.85,
+    equatorialInclination: 25.19,
+    revolutionPeriod: 687 * DAY,
+    rotationCycle: 24 * HOUR + 37 * MINUTE
+  },
+  jupiter: {
+    equatorialRadius: 71492,
+    trackRadius: 5.2026 * AU,
+    orbitalInclination: 1.303,
+    equatorialInclination: 3.08,
+    revolutionPeriod: 11.86 * YEAR,
+    rotationCycle: 9 * HOUR + 50 * MINUTE
+  },
+  saturn: {
+    equatorialRadius: 60268,
+    trackRadius: 9.5549 * AU,
+    orbitalInclination: 2.489,
+    equatorialInclination: 26.7,
+    revolutionPeriod: 29.46 * YEAR,
+    rotationCycle: 10 * HOUR + 39 * MINUTE
+  },
+  uranus: {
+    equatorialRadius: 25559,
+    trackRadius: 19.2184 * AU,
+    orbitalInclination: 0.773,
+    equatorialInclination: 97.9,
+    revolutionPeriod: 84.01 * YEAR,
+    rotationCycle: 17 * HOUR + 14 * MINUTE
+  },
+  neptune: {
+    equatorialRadius: 24764,
+    trackRadius: 30.1104 * AU,
+    orbitalInclination: 1.770,
+    equatorialInclination: 27.8,
+    revolutionPeriod: 164.82 * YEAR,
+    rotationCycle: 16 * HOUR + 06 * MINUTE
+  }
+};
+
+const DATA = {};
+for(let d in data){
+  DATA[d] = new Proxy(data[d], {
+    get: function(target, name){
+      switch(name){
+        case 'equatorialRadius': return target[name] > 100000? target[name] / data.earth.equatorialRadius / 5: target[name] / data.earth.equatorialRadius;
+        case 'trackRadius': return target[name] > 0? 20 + target[name] / data.earth.equatorialRadius / 2000: 0; //target[name] / data.earth.trackRadius * 300;
+        case 'orbitalInclination': return target[name];
+        case 'equatorialInclination': return target[name];
+        case 'revolutionPeriod': return target[name] / 100000;
+        case 'rotationCycle': return target[name] / data.earth.rotationCycle * 100;
+        default: return undefined;
+      }
+    }
+  });
+}
--- index.js
+++ index.js
@@ -1,59 +1,65 @@
 let scene = new THREE.Scene();
 let width = window.innerWidth, height = window.innerHeight;
-let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(15, width / height, 0.1, 9999999999);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
+// renderer.setClearColor(0xffffff)
 document.body.appendChild(renderer.domElement);
 
 let controls = new THREE.OrbitControls(camera);
-
-let sun = createSphere(10, {map: 'assets/solar/sunmap.jpg'});
-let mercury = createSphere(1, {map: 'assets/mercury/mercurymap.jpg', bumpMap: 'assets/mercury/mercurybump.jpg'});
-let venus = createSphere(1, {map: 'assets/venus/venusmap.jpg', bumpMap: 'assets/venus/venusbump.jpg'});
-let earth = createSphere(1, {map: 'assets/earth/earthmap.jpg', bumpMap: 'assets/earth/earthbump.jpg', specularMap: 'assets/earth/earthspec.jpg'});
-let mars = createSphere(1, {map: 'assets/mars/mars.jpg', bumpMap: 'assets/mars/mars_topo.jpg', normalMap: 'assets/mars/mars_normal.jpg'});
-let jupiter = createSphere(1, {map: 'assets/jupiter/jupiter.jpg'});
-let saturn = createSphere(1, {map: 'assets/saturn/saturnmap.jpg'}, {alphaMap: 'assets/saturn/saturnringpattern.gif', map: 'assets/saturn/saturnringcolor.jpg'});
-let uranus = createSphere(1, {map: 'assets/uranus/uranusmap.jpg'}, {alphaMap: 'assets/uranus/uranusringtrans.gif', map: 'assets/uranus/uranusringcolor.jpg'});
-let neptune = createSphere(1, {map: 'assets/neptune/neptunemap.jpg'});
-
-
-let light = new THREE.DirectionalLight(0xffffff);
-// let light = new THREE.AmbientLight(0xffffff);
-light.position.set(100, 100, 100);
-
-camera.position.z = 50;
+camera.position.z = 2000;
 controls.update();
-
-mercury.position.x = 15;
-venus.position.x = 18;
-earth.position.x = 21;
-mars.position.x = 24;
-jupiter.position.x = 27;
-saturn.position.x = 30;
-uranus.position.x = 33;
-neptune.position.x = 36;
-
+console.log(controls)
+// let light = new THREE.DirectionalLight(0xffffff);
+let light = new THREE.PointLight(0xffffff);
+// let light1 = new THREE.PointLight(0xffffff);
+// let light2 = new THREE.PointLight(0xffffff);
+scene.add(new THREE.AmbientLight(0x333333));
+// light.position.set(0, 0, 20);
+// light1.position.set(20, 0, 0);
+// light2.position.set(0, 20, 0);
 scene.add(light);
+// scene.add(light1);
+// scene.add(light2);
+// scene.add(new THREE.HemisphereLight(0xffffff, 0x080820, 1));
 
-scene.add(sun);
-scene.add(mercury);
-scene.add(venus);
-scene.add(earth);
-scene.add(mars);
-scene.add(jupiter);
-scene.add(saturn);
-scene.add(uranus);
-scene.add(neptune);
+// let axesHelper = new THREE.AxesHelper(500);
+// scene.add(axesHelper);
+// scene.add(new THREE.PolarGridHelper(400, 6, 8, 64));
+
+
+let solarSystem = {
+  sun: createSphere(DATA.sun.equatorialRadius, 0, {map: 'assets/solar/sunmap.jpg'}),
+  mercury: createSphere(DATA.mercury.equatorialRadius, 1, {map: 'assets/mercury/mercurymap.jpg', bumpMap: 'assets/mercury/mercurybump.jpg'}),
+  venus: createSphere(DATA.venus.equatorialRadius, 1, {map: 'assets/venus/venusmap.jpg', bumpMap: 'assets/venus/venusbump.jpg'}),
+  earth: createSphere(DATA.earth.equatorialRadius, 1, {map: 'assets/earth/earthmap.jpg', bumpMap: 'assets/earth/earthbump.jpg', specularMap: 'assets/earth/earthspec.jpg'}, null, {map: 'assets/earth/original.jpg', alphaMap: 'assets/earth/earthcloudmaptrans.jpg'}),
+  mars: createSphere(DATA.mars.equatorialRadius, 1, {map: 'assets/mars/mars.jpg', bumpMap: 'assets/mars/mars_topo.jpg', normalMap: 'assets/mars/mars_normal.jpg'}),
+  jupiter: createSphere(DATA.jupiter.equatorialRadius, 1, {map: 'assets/jupiter/jupiter.jpg'}),
+  saturn: createSphere(DATA.saturn.equatorialRadius, 1, {map: 'assets/saturn/saturnmap.jpg'}, {alphaMap: 'assets/saturn/saturnringpattern.gif', map: 'assets/saturn/saturnringcolor.jpg'}),
+  uranus: createSphere(DATA.uranus.equatorialRadius, 1, {map: 'assets/uranus/uranusmap.jpg'}, {alphaMap: 'assets/uranus/uranusringtrans.gif', map: 'assets/uranus/uranusringcolor.jpg'}),
+  neptune: createSphere(DATA.neptune.equatorialRadius, 1, {map: 'assets/neptune/neptunemap.jpg'})
+}
+setSphereData(solarSystem, DATA);
+for(let o in solarSystem){
+  solarSystem[o].arc = 0;
+  scene.add(solarSystem[o]);
+  scene.add(addTrack(DATA[o]))
+}
+window.addEventListener('resize', function(){
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
 
 function animate(){
   requestAnimationFrame(animate);
   controls.update();
+  rotate(solarSystem, DATA);
 
   renderer.render(scene, camera);
 }
 
-function createSphere(radius, textures, ringTextures){
+function createSphere(radius, materialType, textures, ringTextures, outerTextures){
   let materialObj = {
     map: null,
     normalMap: null,
@@ -65,8 +71,9 @@ function createSphere(radius, textures, ringTextures){
     alphaMap: null,
     specularMap: null,
     color: 0xffffff,
-    bumpScale: 0.05,
-    specular: new THREE.Color('grey')
+    bumpScale: 0.02,
+    side: THREE.DoubleSide,
+    specular: new THREE.Color(0x111111)
   };
   let group = new THREE.Group();
   let geometry = new THREE.SphereGeometry(radius, 60, 60);
@@ -93,13 +100,79 @@ function createSphere(radius, textures, ringTextures){
     group.add(ringeMesh);
   }
 
-  let material = new THREE.MeshPhongMaterial(materialObj);
+  // if(outerTextures){
+  //   let geometry = new THREE.SphereGeometry(radius * 1.1, 60, 60);
+  //   let material = new THREE.MeshPhongMaterial({
+  //     map: new THREE.TextureLoader().load(outerTextures.map),
+  //     alphaMap: new THREE.TextureLoader().load(outerTextures.alphaMap),
+  //     side: THREE.DoubleSide,
+  //     transparent: true,
+  //     opacity: 1,
+  //     depthwrite: false
+  //   });
+  //   let mesh = new THREE.Mesh(geometry, material);
+
+  //   group.add(mesh);
+  // }
+
+  let material = null;
+  if(materialType === 0){
+    material = new THREE.MeshBasicMaterial(materialObj);
+  }else{
+    material = new THREE.MeshPhongMaterial(materialObj);
+  }
   let mesh = new THREE.Mesh(geometry, material);
   group.add(mesh);
+  // group.add(new THREE.BoxHelper(mesh.clone(), 0xffffff));
 
   return group;
 }
 
+function setSphereData(obj, data){
+  for(let o in obj){
+    obj[o].rotation.z = data[o].equatorialInclination * Math.PI * 2 / 360; // 自转倾角
+  }
+}
+function rotate(obj, data){
+  for(let o in obj){
+    obj[o].rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI * 2 / data[o].rotationCycle); // 自转
+    // 公转，使用极坐标方程
+    let arc = data[o].revolutionPeriod > 0? Math.PI * 2 / data[o].revolutionPeriod: 0;
+    obj[o].arc += arc;
+    let radius = data[o].trackRadius || 0;
+    obj[o].position.x = radius * Math.sin(obj[o].arc);
+    obj[o].position.z = radius * Math.cos(obj[o].arc);
+    // 公转倾角，使用三角函数
+    obj[o].position.y = obj[o].position.x * Math.tan(Math.PI * 2 / 360 * data[o].orbitalInclination)
+  }
+}
+function addTrack(data){
+  // let mesh = new THREE.Mesh(new THREE.RingGeometry(data.trackRadius - 2, data.trackRadius, 100),
+  //                       new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide}));
+  // mesh.rotation.x = -Math.PI / 2;
+  // // 公转倾角
+  // mesh.rotation.y = -Math.PI * 2 / 360 * data.orbitalInclination;
+  // return mesh;
+  // let curve = new THREE.EllipseCurve(0, 0, 10, 10, 0, 2 * Math.PI, false, 0);
+  var curve = new THREE.EllipseCurve(
+    0,  0,            // ax, aY
+    data.trackRadius, data.trackRadius,           // xRadius, yRadius
+    0,  2 * Math.PI,  // aStartAngle, aEndAngle
+    false,            // aClockwise
+    0                 // aRotation
+  );
 
+  var points = curve.getPoints( 50 );
+  var geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+  var material = new THREE.LineBasicMaterial( { color : 0xffffff } );
+
+  // Create the final object to add to the scene
+  var ellipse = new THREE.Line( geometry, material );
+  ellipse.rotation.x = -Math.PI / 2;
+  // 公转倾角
+  ellipse.rotation.y = -Math.PI * 2 / 360 * data.orbitalInclination;
+  return ellipse;
+}
 
 animate();
